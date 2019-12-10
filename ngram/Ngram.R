694306f93e98dc46941e87ef9b50df4a4f417620
yohan <yohan.alher@gmail.com>
2019-12-10 16:16:04 -0300

    Update Ngram.R

--- ngram/Ngram.R
+++ ngram/Ngram.R
@@ -3,9 +3,9 @@
 #https://github.com/mark-blackmore/JHU-Data-Science-Capstone
 #Thanks Mark really learned a lot and was very helpfull
 
-two_Words <- readRDS("./Final/two_Words.rds")
-tree_Words  <- readRDS("./Final/tree_Words.rds")
-four_Words <- readRDS("./Final/four_Words.rds")
+two_Words <- readRDS("/two_Words.rds")
+tree_Words  <- readRDS("/tree_Words.rds")
+four_Words <- readRDS("/four_Words.rds")
 
 bigram <- function(input_words){
         num <- length(input_words)
